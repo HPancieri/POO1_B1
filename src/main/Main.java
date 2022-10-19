d3d7403c5c25b64a9ec3b77b1495bd2b916640f4
HPancieri <pancieri.trabalhos@gmail.com>
2022-10-19 16:56:53 -0300

    Adicionados os arquivos do trabalho finaizado.

---
+++ src/main/Main.java
@@ -0,0 +1,32 @@
+/***
+ *      _______        _           _ _             _____   ____   ____    __
+ *     |__   __|      | |         | | |           |  __ \ / __ \ / __ \  /_ |
+ *        | |_ __ __ _| |__   __ _| | |__   ___   | |__) | |  | | |  | |  | |
+ *        | | '__/ _` | '_ \ / _` | | '_ \ / _ \  |  ___/| |  | | |  | |  | |
+ *        | | | | (_| | |_) | (_| | | | | | (_) | | |    | |__| | |__| |  | |
+ *        |_|_|  \__,_|_.__/ \__,_|_|_| |_|\___/  |_|     \____/ \____/   |_|
+
+ * Alunos:
+ * 		Hygor Oliveira Pancieri
+ * 		Pâmella Roque
+ * 		João Marcos
+ * Professor:
+ * 		Otavio Lube dos Santos
+ *
+ ***/
+
+package main;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println(" _______        _           _ _             _____   ____   ____    __");
+		System.out.println("|__   __|      | |         | | |           |  __ \\ / __ \\ / __ \\  /_ |");
+		System.out.println("   | |_ __ __ _| |__   __ _| | |__   ___   | |__) | |  | | |  | |  | |");
+		System.out.println("   | | '__/ _` | '_ \\ / _` | | '_ \\ / _ \\  |  ___/| |  | | |  | |  | |");
+		System.out.println("   | | | | (_| | |_) | (_| | | | | | (_) | | |    | |__| | |__| |  | |");
+		System.out.println("   |_|_|  \\__,_|_.__/ \\__,_|_|_| |_|\\___/  |_|     \\____/ \\____/   |_|");
+
+		System.out.println("Alunos:\n\tHygor Oliveira Pancieri\n\tPâmella Roque\n\tJoão Marcos");
+		System.out.println("Professor:\n\tOtavio Lube dos Santos");
+	}
+}
